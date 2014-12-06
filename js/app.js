d6dd103fb3a62fa427dca0e3ccd4d2ed43a965c0
Hoetmaaiers <houdmeyers@gmail.com>
2014-12-06 14:11:15 +0100

    mimick original dots

--- js/app.js
+++ js/app.js
@@ -1,45 +1,77 @@
-(function(){
+// (function(){
 
 var canvas = document.getElementById('myCanvas'),
     ctx = canvas.getContext('2d'),
-    fps = 30,
-    showOriginalDots = false,
-    dotColor = "#599A90";
+    fps = 60,
+    originalDotColor = "#46b6ac",
 
-setupCanvasSize();
+    dots = [];
+
 init();
 
 
 function init() {
+  setupCanvasSize();
   $(window).on('resize', setupCanvasSize);
+
+  setupDots();
+
   render();
+}
+
+
+function setupDots() {
+  var xPosition = -18,
+      yPosition = 16,
+      xSpacing = 35,
+      ySpacing = 27,
+      row = 0;
+
+  while ((yPosition * row) < canvas.height) {
+    xPosition += xSpacing;
+
+    if (xPosition > canvas.width) {
+      row += 1;
+      xPosition = -18;
+    }
+
+    var dot = new Dot(xPosition, yPosition + (ySpacing * row));
+    dots.push(dot);
+  }
 }
 
 function render() {
   setTimeout(function() {
     window.requestAnimationFrame(function() {
 
-      // updateCanvas();
-      // updateObjects();
-      // drawObjects();
+      hideOriginalDots();
 
+      updateDots();
       drawDots();
 
-      displayOriginalDots();
       render();
     });
   }, 1000 / fps);
 };
 
 
-function displayOriginalDots() {
-  ctx.fillStyle = dotColor;
+
+
+function hideOriginalDots() {
+  ctx.fillStyle = originalDotColor;
   ctx.globalAlpha = 0.025;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 }
 
+function updateDots() {}
+
 function drawDots() {
-  // console.log("draw dots");
+  _.forEach(dots, function(dot) {
+    ctx.globalAlpha = 1;
+    ctx.fillStyle = "white";
+    ctx.fillRect(dot.x, dot.y, 2, 2);
+  });
 }
 
 function setupCanvasSize() {
@@ -47,4 +79,4 @@ function setupCanvasSize() {
   canvas.height = window.innerHeight;
 }
 
-}());
+// }());
---
+++ js/dot.js
@@ -0,0 +1,7 @@
+var Dot = function(x, y) {
+  this.color = "red";
+  this.x = x;
+  this.y = y;
+}
+
+Dot.prototype = {}
